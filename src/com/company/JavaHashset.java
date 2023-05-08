a8c4790dab087045f721b869ef05a781e0af0b79
hemanthsahukari <hemanthsahukari59@gmail.com>
2023-05-08 10:28:25 +0530

    first commit

---
+++ src/com/company/JavaHashset.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+class Solution {
+
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+            int t = s.nextInt();
+            String [] pair_left = new String[t];
+            String [] pair_right = new String[t];
+
+            for (int i = 0; i < t; i++) {
+                pair_left[i] = s.next();
+                pair_right[i] = s.next();
+            }
+            HashSet<String> set = new HashSet<String>();
+
+            for (int i = 0; i < pair_left.length;i++){
+                String name = pair_left[i] + " " + pair_right[i];
+                set.add(name);
+                System.out.println(set.size());
+            }
+        }
+    }
+
