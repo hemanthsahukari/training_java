fffef5781b38ac1018d9ca63b34a87732d8bbc96
hemanthsahukari <129752124+hemanthsahukari@users.noreply.github.com>
2023-05-16 16:22:43 +0530

    Update CreateFile.java

--- src/FileHandling/CreateFile.java
+++ src/FileHandling/CreateFile.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class CreateFile {
     public static void main(String[] args) {
         try {
-            File myObj = new File("D:\\FileHandling\\File.txt");
+            File myObj = new File("File.txt");
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
 
